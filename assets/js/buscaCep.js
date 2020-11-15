773aabe8bbf9d0606e87d5ab68536a8bb97954bb
yurimutti <muttiyuri@gmail.com>
2020-11-14 23:53:18 -0300

    Javascript

---
+++ assets/js/buscaCep.js
@@ -0,0 +1,57 @@
+// Aguarda Submit
+document.querySelector('#cepForm').addEventListener('submit', getCepInfo);
+
+function getCepInfo(e) {
+  // Pega valor do cep do input
+  const cep = document.querySelector('.cep').value;
+
+  // Faz a request
+  fetch(`//viacep.com.br/ws/${cep}/json`)
+  .then(response => {
+    return response.json();
+  })
+  .then(data =>{
+    // Exibe informação do CEP
+    let infoCep = "";
+    if(data.erro) {
+    infoCep += `
+    <div class="alertError">
+    <img src="/assets/img/error.svg" alt="Fechar" data-alert="fechar">
+    <p>Cep inexistente.</p>
+    </div>
+    `;
+   } else {
+    infoCep += `
+    <div class="alertCep">
+    <img src="/assets/img/close.svg" alt="Fechar" data-alert="fechar">
+    <h2>Endereço</h2>
+    <p>CEP:<span>${data.cep}</span></p>
+    <p>Logradouro:<span>${data.logradouro}</span></p>
+    <p>Complemento:<span>${data.complemento}</span></p>
+    <p>Bairro:<span>${data.bairro}</span></p>
+    <p>Uf:<span>${data.uf}</span></p>
+  </div>
+    `
+   }
+   
+   // Insere a template no DOM
+   document.querySelector("#infoCep").innerHTML = infoCep;
+
+});
+
+  e.preventDefault();
+
+}
+
+$(document).ready(function(){
+	//CEP
+	$("#cep").mask("99999-999");
+});
+
+const closeAlert = document.querySelector('[data-alert="fechar"]')
+closeAlert.addEventListener('click', close);
+
+function close () {
+  infoCep.style.display = 'none';
+}
+
